8a034b87a4f9f2b2289e997582208ae6c2a9ea19
Paulus Esterhazy <pesterhazy@gmail.com>
2019-04-27 12:04:06 -0500

    WIP

--- src/hooks/core.cljs
+++ src/hooks/core.cljs
@@ -3,11 +3,18 @@
    [goog.dom :as gdom]
    [reagent.core :as r]))
 
+(def use-state js/window.React.useState)
+
 (defn get-app-element []
   (gdom/getElement "app"))
 
 (defn fun-ui []
-  (r/as-element [:div "from reagent"]))
+  (let [[n setN] (use-state 0)]
+    (r/as-element
+     [:div
+      [:p "You clicked " n " times"]
+      [:button {:on-click (fn [] (setN inc))}
+       "click me"]])))
 
 (defn main-ui []
   [:> fun-ui])
